e47027e3c5943197a240ad664cb1ce1eb2799181
joesessions <joesessions@gmail.com>
2021-11-26 19:27:57 -0800

    Update SalesByDayOfWeekPivot.sql

--- ExtraCirricularSQLPractice/SalesByDayOfWeekPivot.sql
+++ ExtraCirricularSQLPractice/SalesByDayOfWeekPivot.sql
@@ -13,26 +13,32 @@ use BerkeleyCrs
 --select datename(weekday,1-2)
 
 --product sales by portion of the alphabet (across the top), year down the side. 
-select 'a-g' as Segment, p.product_name, o.order_date
-from production.products p
-left join sales.order_items oi on p.product_id = oi.product_id
-left join sales.orders o on p.product_id = oi.product_id
+Select * from (
+select year(o.order_date) [year], 'a-g' as Segment, 1 as Sales   
+from sales.order_items oi
+inner join production.products p on p.product_id = oi.product_id
+inner join sales.orders o on p.product_id = oi.product_id
 where substring(p.product_name, 1, 1) >= 'a' and  substring(p.product_name, 1, 1) <= 'g'
-union
-select 'h-m'  as Segment, p.product_name, o.order_date
-from production.products p
-left join sales.order_items oi on p.product_id = oi.product_id
-left join sales.orders o on p.product_id = oi.product_id
+union all
+select year(o.order_date) [year], 'h-m' as Segment, 1 as Sales   
+from sales.order_items oi
+inner join production.products p on p.product_id = oi.product_id
+inner join sales.orders o on p.product_id = oi.product_id
 where substring(p.product_name, 1, 1) >= 'h' and  substring(p.product_name, 1, 1) <= 'm'
-union 
-select 'n-s' as Segment, p.product_name, o.order_date
-from production.products p
-left join sales.order_items oi on p.product_id = oi.product_id
-left join sales.orders o on p.product_id = oi.product_id
+union all
+select year(o.order_date) [year], 'n-s' as Segment, 1 as Sales   
+from sales.order_items oi
+inner join production.products p on p.product_id = oi.product_id
+inner join sales.orders o on p.product_id = oi.product_id
 where substring(p.product_name, 1, 1) >= 'n' and  substring(p.product_name, 1, 1) <= 's'
-union
-select 't-z' as Segment, p.product_name, o.order_date
-from production.products p
-left join sales.order_items oi on p.product_id = oi.product_id
-left join sales.orders o on p.product_id = oi.product_id
+union all
+select year(o.order_date) [year], 't-z' as Segment, 1 as Sales   
+from sales.order_items oi
+inner join production.products p on p.product_id = oi.product_id
+inner join sales.orders o on p.product_id = oi.product_id
 where substring(p.product_name, 1, 1) >= 't' and  substring(p.product_name, 1, 1) <= 'z'
+) pvt
+PIVOT
+(sum(Sales)
+for Segment in ([a-g], [h-m], [n-s], [t-z])) src
+
